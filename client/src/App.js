27608336425f29f33f147296e8414de94bfe8b88
SenorPenguin <jimmy.dwyer25@gmail.com>
2019-03-05 18:27:25 -0600

    office hours preliminary work

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,27 @@ import friends from "./friends.json";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    topscore: 0,
+    score: 0
   };
 
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    // this.setState({ friends });
+    this.setState({friends, score: this.state.score +1});
+   friends.sort(() => Math.random() -0.5);
+   this.state.score++;
+   
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Title>Friends List  score:{this.state.score}</Title>
         {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
@@ -31,6 +37,7 @@ class App extends Component {
             image={friend.image}
             occupation={friend.occupation}
             location={friend.location}
+            saved = {friend.saved}
           />
         ))}
       </Wrapper>
