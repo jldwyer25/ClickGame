06dc54d450bc8e616fe634d15d3495cfb62cf0ed
SenorPenguin <jimmy.dwyer25@gmail.com>
2019-03-07 18:07:30 -0600

    Updates

---
+++ precache-manifest.19a52e72afe9819b2e38775c358cb043.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f093e07922e57e7aafb9",
+    "url": "/ClickGame/static/css/main.1ddecd5f.chunk.css"
+  },
+  {
+    "revision": "f093e07922e57e7aafb9",
+    "url": "/ClickGame/static/js/main.f093e079.chunk.js"
+  },
+  {
+    "revision": "34f4663bc086bb680fa3",
+    "url": "/ClickGame/static/js/runtime~main.34f4663b.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/ClickGame/static/js/2.ff661de6.chunk.js"
+  },
+  {
+    "revision": "7b1d156b53f7c7ad21a2a7f470db8a42",
+    "url": "/ClickGame/index.html"
+  }
+];
